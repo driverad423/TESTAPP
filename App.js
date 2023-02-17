9ed0e98d53ffab427bc4c5f6b7a99ed60012160d
Diego Rivera <diegorivera423@gmail.com>
2023-02-17 17:11:53 -0500

    17-2-2023

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to startdasdsadasd working on your app!</Text>
+      <Text style={styles.titulo}>SEXBET</Text>
+      <Text style={styles.titulo2}>Sign In to your account</Text>
+      <TextInput 
+      placeholder="email"
+      style={styles.textInput}>
+
+      </TextInput>
+      <TextInput placeholder="password"
+      style={styles.textInput}></TextInput>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#011640',
+    //backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 50,
+    color: 'white',
+    //fontWeight: 'bold'
+  
+  },
+
+  titulo2: {
+    fontSize: 12,
+    color: 'gray',
+    //fontWeight: 'bold'
+  
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor: 'gray',
+    borderRadius:30,
+    padding: 10,
+    width:'70%',
+    height:50,
+    marginTop:30,
+    backgroundColor:'white',
+    paddingStart: 20,
+    
+  }
+
 });
